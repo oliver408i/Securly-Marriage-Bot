6e8a0d11981ea491ef97d7fc1a895e426eb5510a
Rufan Yin <75344601+oliver408i@users.noreply.github.com>
2022-01-17 03:58:28 +0000

    explain cuz I forgot

--- Functions/Main/t.py
+++ Functions/Main/t.py
@@ -10,7 +10,7 @@ class t(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-
+    #this gotta be the harest command yet D:
     @commands.command(aliases=['tree', 'familytree', 'gettree'])
     @commands.guild_only()
     async def t(self, ctx, member: discord.Member = None):
@@ -20,7 +20,7 @@ class t(commands.Cog):
             author = member
         db = pickledb.load('database.txt', False)
 
-        class Person:
+        class Person: #here we set up a class and some functions to generate the tree at the end
             ID = itertools.count()
             def __init__(self, name, parent=None, level=0):
                 self.id = next(self.__class__.ID)
@@ -37,30 +37,33 @@ class t(commands.Cog):
                 return member
         if not db.exists(author.name+"#"+author.discriminator+"partner") and not db.exists(author.name+"#"+author.discriminator+"child"):
             return await ctx.send("You have no family :(")
+        #so if everything went fine, then start creating the tree
         await ctx.send("Getting data and creating tree...")
         
         partner = db.get(author.name+"#"+author.discriminator+"partner")
-        def childrenSort(parent):
+        def childrenSort(parent): #this function gets the children of the person in question
             finalChildren = []
-            children = db.get(parent+"child").split("|/")
+            children = db.get(parent+"child").split("|/") #get all the children
             for child in children:
-                if child == '':
+                if child == '': #ghost children idk if it happens now
                     children.remove(child)
-            print(children)
+            print(children) #debug
             for child in children:
-                if child == parent:
+                if child == parent: #geez this shouldn't be a thing...
                     break
+                #in the elifs before, four cases lead to four outputs. The cases are the person have partner + children, only partner, only children, or nothing at all
                 elif db.exists(child+"partner") and db.exists(child+"child"):
                     familytree = {'parent':child+" + " + partner, 'children':childrenSort(child)}
                 elif not db.exists(child+"partner") and db.exists(child+"child"):
-                    familytree = {'parent':child, 'children':childrenSort(child)}
+                    familytree = {'parent':child, 'children':childrenSort(child)} #we called childrenSort again if the child has more children
                 elif db.exists(child+"partner") and not db.exists(child+"child"):
                     familytree = {'parent':child+" + " + partner, 'children':[]}
                 else:
                     familytree = {'parent':child, 'children':[]}
-                finalChildren.append(familytree)
+                finalChildren.append(familytree) #append the family tree
             print(finalChildren)
-            return finalChildren
+            return finalChildren #returns the entire tree
+        #more sorting etc...
         if (not db.exists(author.name+"#"+author.discriminator+"child")) and db.exists(author.name+"#"+author.discriminator+"partner"):
             familytree = {'parent':author.name+"#"+author.discriminator+" + " + partner, 'children':[]}
         elif (not db.exists(author.name+"#"+author.discriminator+"partner")) and db.exists(author.name+"#"+author.discriminator+"child"):
@@ -69,6 +72,7 @@ class t(commands.Cog):
             
             familytree = {'parent':author.name+"#"+author.discriminator+" + " + partner, 'children':childrenSort(author.name+"#"+author.discriminator)}
         print(familytree)
+        #now we can acutally  make the tree
         old_stdout = sys.stdout # Memorize the default stdout stream
         sys.stdout = buffer = io.StringIO()
         t = createTree(familytree)
@@ -85,13 +89,10 @@ class t(commands.Cog):
         printout(t)
         sys.stdout = old_stdout # Put the old stream back in place
         whatWasPrinted = buffer.getvalue() # Return a str containing the entire contents of the buffer.
-        await ctx.send("```"+whatWasPrinted+"```")
+        await ctx.send("```"+whatWasPrinted+"```") #send the tree
         await ctx.send("Use ms!help to see the format of the tree")
+        #GOSH THAT WAS HARD
     
         
-        
-
-
-
 def setup(bot):
     bot.add_cog(t(bot))
